20773a9ed9e76050fd2bc4687dfeb5cf61e7c21c
Stok <sean@stokedprojects.org>
2018-05-11 23:46:31 +0200

    removed all cd action

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -17,13 +17,11 @@ sudo systemctl restart php7.0-fpm
 
 # Download wp
 mkdir tmp
-cd /tmp
 curl -O https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
-cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
-mkdir ./wordpress/wp-content/upgrade
-sudo cp -a ./wordpress/. /var/www/html
-cd ..
+cp ./tmp/wordpress/wp-config-sample.php ./tmp/wordpress/wp-config.php
+mkdir ./tmp/wordpress/wp-content/upgrade
+sudo cp -a ./tmp/wordpress/. /var/www/html
 
 # Configure the WordPress Directory
 sudo chown -R $(whoami):www-data /var/www/html
